8fe88bc1d362e9fe0dc1f1d683ffc318f38a9ebd
Ali Tahvildari <a.tahvildari1997@gmail.com>
2023-01-21 15:35:01 +0100

    first commit

---
+++ Runners/src/App.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div>hi</div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
